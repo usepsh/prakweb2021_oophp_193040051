ac41f25df55b7505eb0788e750a0cb4081bce43b
usepsh <58686569+usepsh@users.noreply.github.com>
2021-09-16 10:18:52 +0700

    Membuat property dan method

--- produk.php
+++ produk.php
@@ -1,11 +1,37 @@
-<?php 
+<?php
 
 // Jualan produk
 // Komik
 // Game
-class Produk{
+class Produk
+{
+  public $judul,
+    $penulis,
+    $penerbit,
+    $harga;
 
+  public function getLabel()
+  {
+    return "$this->penulis, $this->penerbit";
+  }
 }
 
-$produk1 = new Produk();
-$produk2 = new Produk();
+// $produk1 = new Produk();
+// $produk1->judul = "Naruto";
+// var_dump($produk1);
+
+$produk3 = new Produk();
+$produk3->judul = "Naruto";
+$produk3->penulis = "Masashi Kishimoto";
+$produk3->penerbit = "Shonen jump";
+$produk3->harga = 30000;
+
+$produk4 = new Produk();
+$produk4->judul = "Uncharted";
+$produk4->penulis = "Neil Druckmann";
+$produk4->penerbit = "Sony Computer";
+$produk4->harga = 250000;
+
+echo "Komik : " . $produk3->getLabel();
+echo "<br>";
+echo "Game : " . $produk4->getLabel();
