fca26d6727ebae6a9e117eccabc20d53e4eedeb0
usepsh <58686569+usepsh@users.noreply.github.com>
2021-09-23 09:56:49 +0700

    Mempelajari tentang konsep overriding

--- overriding.php
+++ overriding.php
@@ -5,18 +5,14 @@ class Produk
   public $judul,
     $penulis,
     $penerbit,
-    $harga,
-    $jmlHalaman,
-    $waktuMain;
+    $harga;
 
-  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0, $waktuMain = 0)
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
   {
     $this->judul = $judul;
     $this->penulis = $penulis;
     $this->penerbit = $penerbit;
     $this->harga = $harga;
-    $this->jmlHalaman = $jmlHalaman;
-    $this->waktuMain = $waktuMain;
   }
 
   public function getLabel()
@@ -31,8 +27,17 @@ class Produk
   }
 }
 
+
 class Komik extends Produk
 {
+  public $jmlHalaman;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $jmlHalaman = 0)
+  {
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->jmlHalaman = $jmlHalaman;
+  }
+
   public function getInfoProduk()
   {
     $str = "Komik : " . parent::getInfoProduk() . " - {$this->jmlHalaman} Halaman.";
@@ -40,14 +45,24 @@ class Komik extends Produk
   }
 }
 
+
 class Game extends Produk
 {
-  public function getInfoProduk()
+  public $waktuMain;
+
+  public function __construct($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0, $waktuMain = 0)
   {
-    $str = "Game : {$this->judul} | {$this->getLabel()} (Rp. {$this->harga}) - {$this->waktuMain} Jam.";
+    parent::__construct($judul, $penulis, $penerbit, $harga);
+    $this->waktuMain = $waktuMain;
+  }
+
+  public function getInfoProduk($judul = "judul", $penulis = "penulis", $penerbit = "penerbit", $harga = 0)
+  {
+    $str = "Game : " . parent::getInfoProduk() . " - {$this->waktuMain} Jam.";
     return $str;
   }
 }
+
 
 class CetakInfoProduk
 {
@@ -58,8 +73,8 @@ class CetakInfoProduk
   }
 }
 
-$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100, 0);
-$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 0, 50,);
+$produk1 = new Komik("Naruto", "Masashi Kishimoto", "Shonen Jump", 30000, 100);
+$produk2 = new Game("Uncharted", "Neil Druckmann", "Sony Computer", 250000, 50);
 
 // Komik : Naruto | Masashi Kishimoto , Shonen Jump (Rp. 30000) - 100 Halaman.
 // Game : Uncharted | Neil Druckmann , Sony Computer (Rp. 250000) - 50 Jam.
